b8b68b7b1490111013cc08b408e7556b14313383
Ayush Pandey <ayushpaandey.01@gmail.com>
2024-04-22 18:18:50 +0530

    Resolved the Json error

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -10,15 +10,15 @@ const HomePage = async () => {
     const products = await getProducts({isFeatured:true});
     const billboard = await getBillboard("ebcf1769-0e59-4db1-a0d7-3ab4c6bdcde6");
     return ( 
+        
         <Container>
-            <div className="space-y-10 pb-10">
-                <Billboard data={billboard}/>
-                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                    <ProductList title= "Featured Products" items={products} />
-                </div>
-            </div>
-
-        </Container>
+        <div className="space-y-10 pb-10">
+          <Billboard data={billboard} />
+        </div>
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </Container>
      );
 }
  
